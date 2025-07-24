f23f032a5e5b4d76c9f4adc821ab2ceef52179d5
Schwitzer <schwiete.ben2@gmail.com>
2025-07-24 11:14:46 +0200

    Add delete_files function and adjust the delete parser

--- pyfile/cli.py
+++ pyfile/cli.py
@@ -14,12 +14,12 @@ delete_parser = subparsers.add_parser(
     'delete',
     aliases=['del'],
     help='Delete files being part of the given directories based on specified conditions',
-    usage='pyfile delete <dir_path_1> ... <dir_path_i> [--size MIN MAX] [--name MIN MAX] [--file_types FILE_TYPE_1 ... FILE_TYPE_i] [--date DD.MM.YY  DD.MM.YY] [--only_empty]',
+    usage='pyfile delete <dir_path_1> ... <dir_path_i> [--size MIN MAX] [--name MIN MAX] [--date DD.MM.YY  DD.MM.YY] [--file_types FILE_TYPE_1 ... FILE_TYPE_i] [--only_empty]',
 )
 
 delete_parser.add_argument('dir_paths', nargs='+', help='Directories that will be scanned for files to be deleted', type=str)
 delete_parser.add_argument('--size', nargs=2, metavar=('MIN', 'MAX'), type=int, help='Delete files whose size is within the given interval', default=None)
 delete_parser.add_argument('--name', nargs=2, metavar=('MIN', 'MAX'), type=int, help='Delete files whose name length is within the given interval', default=None)
+delete_parser.add_argument('--date', nargs=2, metavar=('START', 'END'), help='Delete files created within the given interval', default=None)
 delete_parser.add_argument('--file_types', nargs='+', metavar=('types', 'type_1'), help='Files whose type is not in the list will be ignored', default=None)
-delete_parser.add_argument('--date', nargs=2, metavar=('START', 'END'), help='Delete files modified within the given date range (YYYY-MM-DD)', default=None)
 delete_parser.add_argument('--only_empty', action='store_true', help='Ignore sizing conditions and delete only empty files')
---
+++ pyfile/file_operations.py
@@ -0,0 +1,7 @@
+"""
+This module provides functions for file operations
+"""
+
+def delete_files(dir_paths: list[str], size_interval: tuple[int, int] | None = None, name_interval: tuple[int, int] | None = None,
+                 date_interval: tuple[str, str] | None = None, file_types: list[str] | None = None, only_empty: bool = False) -> None:
+    pass
