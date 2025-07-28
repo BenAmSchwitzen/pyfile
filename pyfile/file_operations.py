3642ddf0970554f84f59ec63472fdeb390d3376e
Schwitzer <schwiete.ben2@gmail.com>
2025-07-28 08:37:29 +0200

    Refactor filter_files function and add helper function to filter files

--- pyfile/file_operations.py
+++ pyfile/file_operations.py
@@ -49,7 +49,7 @@ def delete_files(dir_paths: list[str], size_interval: tuple[int, int] | None = N
 
 def filter_files(dir_path: str | Path, size_interval: tuple[int, int] | None = None, name_interval: tuple[int, int] | None = None,
                  date_interval: tuple[str, str] | None = None, file_types: list[str] | None = None, only_empty: bool = False, 
-                 mark: str | None = None, move: str | None = None, delete: bool = False) -> None:
+                 mark: str | None = None, move: str | None = None, delete: bool = False, ) -> None:
     """
     Filter files based on specified conditions and perform a specified action
 
@@ -62,37 +62,7 @@ def filter_files(dir_path: str | Path, size_interval: tuple[int, int] | None = N
     if sum(filter_actions) > 1:
         raise ValueError('You are not allowed to specify more than one of the following actions: mark, move, delete')
 
-    filtered_files: list[Path] = []
-
-    if not isinstance(dir_path, Path):
-        dir_path = Path(dir_path)
-
-    if not dir_path.is_dir():
-        raise NotADirectoryError(f'{dir_path} is not a directory')
-    
-    if date_interval:
-        date_bounds = [utils.get_file_modification_date_timestamp(date_str) for date_str in date_interval]
-    else:
-        date_bounds = None 
-
-    for file_path in dir_path.glob('**/*'):
-            file_path_stat = file_path.stat()
-
-            if only_empty:
-                if file_path_stat.st_size != 0:
-                    continue
-            else:
-                if size_interval and not size_interval[0] <= file_path_stat.st_size <= size_interval[1]:
-                    continue
-            if name_interval and not name_interval[0] <= utils.get_file_name_length(file_path) <= name_interval[1]:
-                continue
-            if date_bounds:
-                if not date_bounds[0] <= file_path_stat.st_mtime <= date_bounds[1]:
-                    continue
-            if file_types and not utils.get_file_type(file_path) in file_types:
-                continue
-
-            filtered_files.append(file_path)
+    filtered_files: list[Path] = utils.get_filtered_files(dir_path, size_interval, name_interval, date_interval, file_types, only_empty)
 
     if mark:
         for file_path in filtered_files:
--- pyfile/utils.py
+++ pyfile/utils.py
@@ -47,3 +47,42 @@ def get_file_type(file_path: str | Path) -> str:
         file_path = Path(file_path)
     return file_path.suffix
 
+
+def get_filtered_files(dir_path: str | Path, size_interval: tuple[int, int] | None = None, name_interval: tuple[int, int] | None = None,
+                 date_interval: tuple[str, str] | None = None, file_types: list[str] | None = None, only_empty: bool = False) -> list[Path]:
+    """
+    Return a list containing files being part of the given directory and meeting the specified conditions
+    """
+    filtered_files: list[Path] = []
+
+    if not isinstance(dir_path, Path):
+        dir_path = Path(dir_path)
+
+    if not dir_path.is_dir():
+        raise NotADirectoryError(f'{dir_path} is not a directory')
+    
+    if date_interval:
+        date_bounds = [get_file_modification_date_timestamp(date_str) for date_str in date_interval]
+    else:
+        date_bounds = None 
+
+    for file_path in dir_path.glob('**/*'):
+            file_path_stat = file_path.stat()
+
+            if only_empty:
+                if file_path_stat.st_size != 0:
+                    continue
+            else:
+                if size_interval and not size_interval[0] <= file_path_stat.st_size <= size_interval[1]:
+                    continue
+            if name_interval and not name_interval[0] <= get_file_name_length(file_path) <= name_interval[1]:
+                continue
+            if date_bounds:
+                if not date_bounds[0] <= file_path_stat.st_mtime <= date_bounds[1]:
+                    continue
+            if file_types and not get_file_type(file_path) in file_types:
+                continue
+
+            filtered_files.append(file_path)
+
+    return filtered_files
